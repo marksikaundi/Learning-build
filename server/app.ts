0eb34f1540d4251c2e72e66bd505b0989b705806
marksikaundi <emmanuelmark117@gmail.com>
2023-11-24 16:10:54 +0200

    test app endpoint connected successfully

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,33 @@
-import express from 'express';
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+
+//cookie parser
+app.use(cookieParser());
+
+//cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//Test API
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working fine",
+  });
+});
+
+//unknown route handler
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Route ${req.originalUrl} not found`) as any;
+  error.statusCode = 404;
+  next(error);
+});
